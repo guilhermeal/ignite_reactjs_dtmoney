58d5143116f9018035468c56febc1917b75acb2d
Guilherme Almeida <guiallan.al@gmail.com>
2022-01-13 10:52:21 -0300

    list transactions on table from api miragejs

--- src/index.tsx
+++ src/index.tsx
@@ -8,11 +8,34 @@ createServer({
     transaction: Model,
   },
 
+  seeds(server) {
+    server.db.loadData({
+      transactions: [
+        {
+          id: 1,
+          title: "Desenvolvimento de website",
+          type: "deposit",
+          category: "Development",
+          amount: 6000,
+          createdAt: new Date("2021-12-10 10:15:00"),
+        },
+        {
+          id: 2,
+          title: "Financiamento Aptº",
+          type: "withdraw",
+          category: "Despesas Fixas",
+          amount: 1600,
+          createdAt: new Date("2021-12-20 20:15:00"),
+        },
+      ],
+    });
+  },
+
   routes() {
     this.namespace = "api";
 
     this.get("/transactions", () => {
-      return this.schema.all("transactions");
+      return this.schema.all("transaction");
     });
 
     this.post("/transactions", (schema, request) => {
